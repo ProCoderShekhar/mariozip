694d93d9603c917a8ede2fc485f38436b8160a3e
ProCoderShekhar <100035814+ProCoderShekhar@users.noreply.github.com>
2026-02-22 19:05:57 +0530

    Add allowedHosts to Vite server configuration
    Added allowedHosts configuration for the Vite server.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: Number(process.env.PORT) || 5173,
+
+    // ✅ ADD THIS
+    allowedHosts: [
+      'mariozip.com'
+    ],
+
     proxy: {
       '/api/connect': {
         target: 'https://roobetconnect.com',
